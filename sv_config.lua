a28aad945f7b060be3c799fd035b806de35c548a
Randolio <nobanpls@mail.com>
2024-03-20 16:15:40 +0000

    Update sv_config.lua

--- sv_config.lua
+++ sv_config.lua
@@ -1,7 +1,7 @@
 return {
     Timeout = 3,
     Locations = {
-        {price = 500, coords = vec4(-183.8338, -721.8452, 454.9005, 286.1612), busy = true},
+        {price = 500, coords = vec4(-183.8338, -721.8452, 454.9005, 286.1612), busy = false},
         {price = 250, coords = vec4(-906.7607, 4637.28, 883.7651, 252.7173), busy = false},
         {price = 750, coords = vec4(514.6714, 1705.952, 940.7874, 254.3951), busy = false},
     },
